4647f298974ef60af1bcaa8ee5358a44b94ef556
Fzhiyu <2403809502@qq.com>
2024-11-23 22:28:35 +0800

    #fzhiyu 初始化springBoot设置

--- src/main/java/com/xiaofeng/strangeloop/StrangeLoopApplication.java
+++ src/main/java/com/xiaofeng/strangeloop/StrangeLoopApplication.java
@@ -1,11 +1,12 @@
 package com.xiaofeng.strangeloop;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@MapperScan("com.xiaofeng.strangeloop.system.mapper")
 public class StrangeLoopApplication {
 
     public static void main(String[] args) {
