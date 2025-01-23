8b0590bcd0f3283e13edc61d85a1a911ba24f0cf
Fzhiyu <2403809502@qq.com>
2025-01-23 14:24:54 +0800

    #fzhiyu ModelConversation设定id自增

--- src/main/java/com/xiaofeng/strangeloop/system/domain/ModelConversation.java
+++ src/main/java/com/xiaofeng/strangeloop/system/domain/ModelConversation.java
@@ -1,5 +1,7 @@
 package com.xiaofeng.strangeloop.system.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -7,6 +9,8 @@ import java.util.Arrays;
 
 @Data
 public class ModelConversation {
+    //使表id手动生成
+    @TableId(value = "id", type = IdType.INPUT)
     private Integer id;
     private Integer userId;
     private String title;
