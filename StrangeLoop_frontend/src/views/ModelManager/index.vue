b61468b603317234458562f14cfebec4855341b0
Fzhiyu <2403809502@qq.com>
2025-03-11 01:23:57 +0800

    #fzhiyu 前端模型设置完成设置导入到ollama功能

--- StrangeLoop_frontend/src/views/ModelManager/index.vue
+++ StrangeLoop_frontend/src/views/ModelManager/index.vue
@@ -5,18 +5,25 @@
     <div class=" bg-white p-5 rounded-lg shadow-md w-full  overflow-y-scroll">
       <!-- 模型信息区 -->
       <div class="flex justify-start">
-        <div class="w-24 h-24 bg-gray-200 rounded-full flex items-center justify-center text-3xl font-bold text-gray-600">
+        <div
+            class="w-24 h-24 bg-gray-200 rounded-full flex items-center justify-center text-3xl font-bold text-gray-600">
           AI
         </div>
         <div class="flex-1 text-left">
           <h2 class="text-2xl font-bold text-gray-900 mb-2">模型名称:{{ modelInfoMange.modelName }} </h2>
           <p class="text-gray-600 mb-1">模型描述: {{ modelInfoMange.description }}</p>
-          <p v-if="modelInfoMange.linkType==0" class="text-gray-600 mb-1">基础模型: {{  modelInfoMange.localmodelName }}</p>
-          <p v-if="modelInfoMange.linkType==1" class="text-gray-600 mb-1">基础模型: {{  modelInfoMange.modelAiOnline.aiName }}</p>
-          <p v-if="modelInfoMange.linkType==0" class="text-gray-600 mb-1">模型版本: {{ modelInfoMange.localModelInfo.details.parameter_size }}</p>
-          <p v-if="modelInfoMange.linkType==1" class="text-gray-600 mb-1">AI类型: {{ modelInfoMange.modelAiOnline.aiType ===1?'LLM': modelInfoMange.modelAiOnline.aiType ===2?'CHAT':'IMAGE' }}</p>
-<!--          暂时注释 之后增加字段-->
-<!--          <p class="text-gray-600">创建时间: {{ modelStore.currModelInfo.aiOnline.createdTime  }}</p>-->
+          <p v-if="modelInfoMange.linkType==0" class="text-gray-600 mb-1">基础模型: {{
+              modelInfoMange.localmodelName
+            }}</p>
+          <p v-if="modelInfoMange.linkType==1" class="text-gray-600 mb-1">基础模型:
+            {{ modelInfoMange.modelAiOnline.aiName }}</p>
+          <p v-if="modelInfoMange.linkType==0" class="text-gray-600 mb-1">模型版本:
+            {{ modelInfoMange.localModelInfo.details.parameter_size }}</p>
+          <p v-if="modelInfoMange.linkType==1" class="text-gray-600 mb-1">AI类型: {{
+              modelInfoMange.modelAiOnline.aiType === 1 ? 'LLM' : modelInfoMange.modelAiOnline.aiType === 2 ? 'CHAT' : 'IMAGE'
+            }}</p>
+          <!--          暂时注释 之后增加字段-->
+          <!--          <p class="text-gray-600">创建时间: {{ modelStore.currModelInfo.aiOnline.createdTime  }}</p>-->
         </div>
       </div>
 
@@ -26,47 +33,65 @@
         <div class="grid grid-cols-2 gap-6">
           <div>
             <label class="block text-sm font-medium text-gray-700 mb-1" for="system-prompt">系统提示词</label>
-            <textarea id="system-prompt" v-model="modelInfoMange.modelFile.system" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" >{{ modelInfoMange.modelFile.system }}</textarea>
+            <textarea id="system-prompt" v-model="modelInfoMange.modelFile.system"
+                      class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">{{ modelInfoMange.modelFile.system }}</textarea>
           </div>
           <div>
             <label class="block text-sm font-medium text-gray-700 mb-1" for="mirostat">Mirostat</label>
-            <select id="mirostat" v-model="modelInfoMange.modelFile.mirostat" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+            <select id="mirostat" v-model="modelInfoMange.modelFile.mirostat"
+                    class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
               <option value="0">关闭</option>
               <option value="1">启用1.0</option>
               <option value="2">启用2.0</option>
             </select>
           </div>
           <div>
-            <label class="block text-sm font-medium text-gray-700 mb-1" for="mirostat-learning-rate">Mirostat学习速率</label>
-            <input id="mirostat-learning-rate" v-model="modelInfoMange.modelFile.mirostatEat" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" max="1" min="0" step="0.1" type="number">
+            <label class="block text-sm font-medium text-gray-700 mb-1"
+                   for="mirostat-learning-rate">Mirostat学习速率</label>
+            <input id="mirostat-learning-rate" v-model="modelInfoMange.modelFile.mirostatEat"
+                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                   max="1" min="0" step="0.1" type="number">
           </div>
           <div>
             <label class="block text-sm font-medium text-gray-700 mb-1" for="mirostat-entropy">Mirostat熵值</label>
-            <input id="mirostat-entropy" v-model="modelInfoMange.modelFile.mirostatTau" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" max="10" min="0" step="0.1" type="number">
+            <input id="mirostat-entropy" v-model="modelInfoMange.modelFile.mirostatTau"
+                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                   max="10" min="0" step="0.1" type="number">
           </div>
           <div>
             <label class="block text-sm font-medium text-gray-700 mb-1" for="context-length">上下文长度</label>
-            <input id="context-length" v-model="modelInfoMange.modelFile.numCtx" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" min="0" step="1" type="number">
+            <input id="context-length" v-model="modelInfoMange.modelFile.numCtx"
+                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                   min="0" step="1" type="number">
           </div>
           <div>
             <label class="block text-sm font-medium text-gray-700 mb-1" for="repeat-check-length">检查重复长度</label>
-            <input id="repeat-check-length" v-model="modelInfoMange.modelFile.repeatLastN" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" min="0" step="1" type="number">
+            <input id="repeat-check-length" v-model="modelInfoMange.modelFile.repeatLastN"
+                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                   min="0" step="1" type="number">
           </div>
           <div>
             <label class="block text-sm font-medium text-gray-700 mb-1" for="repeat-penalty">重复token惩罚</label>
-            <input id="repeat-penalty" v-model="modelInfoMange.modelFile.repeatPenalty" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" min="1" step="0.1" type="number">
+            <input id="repeat-penalty" v-model="modelInfoMange.modelFile.repeatPenalty"
+                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                   min="1" step="0.1" type="number">
           </div>
           <div>
             <label class="block text-sm font-medium text-gray-700 mb-1" for="output-randomness">输出随机度</label>
-            <input id="output-randomness" v-model="modelInfoMange.modelFile.temperature" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" max="1" min="0" step="0.1" type="number">
+            <input id="output-randomness" v-model="modelInfoMange.modelFile.temperature"
+                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                   max="1" min="0" step="0.1" type="number">
           </div>
         </div>
         <div class="mt-4 flex items-center">
           <label class="flex items-center" for="tail-free-sampling">
-            <input id="tail-free-sampling" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" type="checkbox">
+            <input id="tail-free-sampling" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                   type="checkbox">
             <span class="ml-2 text-sm text-gray-600">Tail Free Sampling</span>
           </label>
-          <input id="tail-free-sampling-value" v-model="modelInfoMange.modelFile.tfsZ" class="ml-4 w-16 focus:ring-indigo-500 focus:border-indigo-500 shadow-sm sm:text-sm border-gray-300 rounded-md" max="1" min="0" step="0.1" type="number">
+          <input id="tail-free-sampling-value" v-model="modelInfoMange.modelFile.tfsZ"
+                 class="ml-4 w-16 focus:ring-indigo-500 focus:border-indigo-500 shadow-sm sm:text-sm border-gray-300 rounded-md"
+                 max="1" min="0" step="0.1" type="number">
         </div>
         <div class="mt-4">
           <label class="block text-sm font-medium text-gray-700 mb-1" for="creativity-level">创意等级</label>
@@ -82,20 +107,32 @@
       <div class="mt-8">
         <h3 class="text-lg font-medium text-gray-900 mb-4">样本回复</h3>
         <div class="overflow-x-auto">
-          <button class="bg-blue-50 flex left-0 hover:bg-blue-100 text-blue-700 font-bold py-2 px-4 rounded-md mt-4" @click="addModelFileEgmessage">添加样本回复</button>
+          <button class="bg-blue-50 flex left-0 hover:bg-blue-100 text-blue-700 font-bold py-2 px-4 rounded-md mt-4"
+                  @click="addModelFileEgmessage">添加样本回复
+          </button>
           <table class="min-w-full  divide-y  divide-gray-200">
             <thead class="bg-gray-50">
             <tr>
-              <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" scope="col">序号</th>
-              <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" scope="col">客户消息</th>
-              <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" scope="col">模型回复</th>
+              <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" scope="col">
+                序号
+              </th>
+              <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" scope="col">
+                客户消息
+              </th>
+              <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" scope="col">
+                模型回复
+              </th>
             </tr>
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
             <tr v-for="(item,index) in modelInfoMange.modelFile.modelFileEgmessageList" :key="index" class="text-left">
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500" contenteditable="true">{{item.messageId}}</td>
-              <td class="px-6 py-4 text-sm text-gray-500" contenteditable="true">{{item.userMessage}}</td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium" contenteditable="true">{{item.modelMessage}}</td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500" contenteditable="true">
+                {{ item.index }}
+              </td>
+              <td class="px-6 py-4 text-sm text-gray-500" contenteditable="true">{{ item.userMessage }}</td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium" contenteditable="true">
+                {{ item.modelMessage }}
+              </td>
             </tr>
             </tbody>
           </table>
@@ -104,7 +141,9 @@
 
       <!-- 保存按钮 -->
       <div class="mt-8 flex justify-center">
-        <button class="!rounded-button whitespace-nowrap inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" type="button" @click="clickSaveModelInfo">
+        <button
+            class="!rounded-button whitespace-nowrap inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            type="button" @click="clickSaveModelInfo">
           保存设置
         </button>
       </div>
@@ -124,39 +163,56 @@ import {updateModelInfoAndModelFile} from "../../api/manageDetail.ts";
 import {ElMessage} from "element-plus";
 import {addMessage} from "../../api/message.ts";
 import router from "../../router";
+import {OllamaRequest} from "../../utils/OllamaRequest.ts";
 // 这里可以添加需要的响应式数据和方法
 const creativityLevel = ref(5.0);
 
 const route = useRoute();
 
 const modelId = ref<string | undefined>(route.query.conversation_id as string);
-const modelInfoMange = ref<ModelInfoMangeType|null>();
+const modelInfoMange = ref<ModelInfoMangeType | null>();
+const ollamaRequest = new OllamaRequest();
+const ollamaModelFile= ref({
+  model: "exampleModelName", // 必需字段
+  from: "exampleSourceName", // 必需字段
+  system: "exampleSystemConfig", // 可选字段，可以省略或者提供具体值
+  parameters: { // 可选字段，可以省略或者提供具体值
+    mirostat: null, // 示例值，可选
+    mirostat_eta: null, // 示例值，可选
+    mirostat_tau: null, // 示例值，可选
+    num_ctx: null, // 示例值，可选
+    repeat_last_n: null, // 示例值，可选
+    repeat_penalty: null, // 示例值，可选
+    temperature: null, // 示例值，可选
+    seed: null ,// 示例值，可选
+    stop: "stopToken", // 示例值，可选，可以是单个字符串或字符串数组
+    num_predict: null, // 示例值，可选
+    top_k: null, // 示例值，可选
+    top_p: null, // 示例值，可选
+    min_p: null // 示例值，可选
+  },
+  template: null
+})
 const modelStore = useModelStore();
 
-// watchEffect(()=>{
-//   if(route.query.modelId){
-//     modelId.value = route.query.modelId as string;
-//     initManager();
-//   }
-// })
 watch(() => route.query.modelId, (id) => {
   modelId.value = id as string;
   initManager();
   // 当路由参数变化时调用initConversation函数
-},{deep:true});
+}, {deep: true});
 
-onMounted(() =>{
-    modelId.value = route.query.modelId as string
-    initManager();
+onMounted(() => {
+  modelId.value = route.query.modelId as string
+  initManager();
 })
 // 初始化设置面板
-const initManager = async ()=>{
+const initManager = async () => {
   console.log("initManager");
   modelStore.indexModelManager = modelId.value;
   modelStore.modelIndex = 1;
-  let modelInfo = await getModelInfo({id:modelId.value});
+  let modelInfo = await getModelInfo({id: modelId.value});
   if (modelInfo.localmodelName) {
-    modelInfo.localModelInfo=await showOllama(modelInfo.localmodelName);
+    modelInfo.localModelInfo = await showOllama(modelInfo.localmodelName);
   }
   modelInfoMange.value = modelInfo;
   console.log(modelInfoMange.value)
@@ -170,13 +226,28 @@ const showOllama = async (model) => {
   return res;
 }
 
+// 初始化ollamaModelFile
+const initOllamaModelFile=()=>{
+  ollamaModelFile.value.model = modelInfoMange.value.ollamaModelName;
+  ollamaModelFile.value.from = modelInfoMange.value.localmodelName;
+  ollamaModelFile.value.system = modelInfoMange.value.modelFile.system;
+  ollamaModelFile.value.parameters = modelInfoMange.value?.modelFile;
+  delete ollamaModelFile.value.parameters.modelFileEgmessageList;
+  console.log(ollamaModelFile.value)
+}
 
-const clickSaveModelInfo= async ()=>{
+const clickSaveModelInfo = async () => {
   console.log(modelInfoMange.value);
+  initOllamaModelFile();
+  if(modelInfoMange.value.modelFile.modelFileEgmessageList==undefined){
+    modelInfoMange.value.modelFile.modelFileEgmessageList = [];
+  }
+
+  await ollamaRequest.sendOllamaCreatedModel(ollamaModelFile.value);
   let res = await updateModelInfoAndModelFile({data: modelInfoMange.value});
-  if(res.data.code === 200){
+  if (res.data.code === 200) {
     ElMessage.success("设置导入成功")
-  }else {
+  } else {
     ElMessage.error("设置导入失败")
   }
 }
@@ -185,67 +256,91 @@ const addModelFileEgmessage = () => {
   const listItem = new MessageList()
   modelInfoMange.value.modelFile.modelFileEgmessageList.push(listItem)
 }
-onMounted(()=>{
+onMounted(() => {
 
 })
 
-interface ModelInfoMangeType{
+// 暂时弃用
+interface OllamaModelFile {
+  model: string;
+  from: string;
+  system?: string;
+  parameters?: {
+    mirostat?: 0 | 1 | 2;
+    mirostat_eta?: number;
+    mirostat_tau?: number;
+    num_ctx?: number;
+    repeat_last_n?: number;
+    repeat_penalty?: number;
+    temperature?: number;
+    seed?: number;
+    stop?: string | string[];
+    num_predict?: number;
+    top_k?: number;
+    top_p?: number;
+    min_p?: number;
+  };
+  template: string;
+}
+
+interface ModelInfoMangeType {
   aiolId: any
   baseModelName: any
   description: any
   disable: any
   linkType: any
+  ollamaModelName:string
   localModelInfo: {
-    details:any
-    model_info:any
-    modelfile:any
+    details: any
+    model_info: any
+    modelfile: any
     modified_at: any
     parameters: any
     system: any
-    template:any
+    template: any
   } | null
   localmodelName: any
   modelAiOnline: any
   modelFile: {
     minP: any
     mirostat: any
-    mirostatEat:any
+    mirostatEat: any
     mirostatTau: any
     system: any
-    modelFileEgmessageList: any|[]
-modelFileId:any
-numCtx:any
-numPredict:any
-repeatLastN:any
-repeatPenalty:any
-seed:any
-stop:any
-temperature:any
-tfsZ:any
-topP:any} |any
-  modelFileId:any
-  modelId:any
+    modelFileEgmessageList: any | []
+    modelFileId: any
+    numCtx: any
+    numPredict: any
+    repeatLastN: any
+    repeatPenalty: any
+    seed: any
+    stop: any
+    temperature: any
+    tfsZ: any
+    topP: any
+  } | any
+  modelFileId: any
+  modelId: any
   modelName: any
   modelVersion: any
   userId: any
 }
+
 class MessageList {
-  messageId:string | number
+  index: string | number
   modelFileId: string | number
-  modelMessage: string |""
-  userMessage:string|""
+  modelMessage: string | ""
+  userMessage: string | ""
 
-  addMessage:()=>{
-
-}
+  addMessage: () => {}
 
   constructor() {
     try {
       const length = modelInfoMange.value.modelFile.modelFileEgmessageList.length
       const item = modelInfoMange.value.modelFile.modelFileEgmessageList[length - 1]
-      this.messageId = item.messageId +1
-    }catch (e) {
-      this.messageId = 1
+      this.index = item.messageId + 1
+    } catch (e) {
+      this.index = 1
     }
     this.modelFileId = modelInfoMange.value.modelFile.modelFileId
   }
